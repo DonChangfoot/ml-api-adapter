64246afc3ae284ec139288eacc0482566850c8fd
Joran Dirk Greef <joran@ronomon.com>
2020-05-21 18:23:09 +0200

    Set consumeTimeout=13000ms

--- static-repo/central-services-stream-9.2.0/src/kafka/consumer.js
+++ static-repo/central-services-stream-9.2.0/src/kafka/consumer.js
@@ -45,7 +45,7 @@ global.LEV.PORT = 4444
 // coil-perf:
 const CONCURRENCY = 1
 const KAFKA_BATCH_COUNT = 7
-const KAFKA_BATCH_TIMEOUT = 3000
+const KAFKA_BATCH_TIMEOUT = 13000
 
 const EventEmitter = require('events')
 const async = require('async')
