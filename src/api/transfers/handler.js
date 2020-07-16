1b2e778a9742eb91a026928996d3b9f16c0670ae
Joran Dirk Greef <joran@ronomon.com>
2020-07-16 17:59:36 +0200

    Return a 203 instead of 202 to test that code changes are effective through to our test harness

--- src/api/transfers/handler.js
+++ src/api/transfers/handler.js
@@ -109,7 +109,7 @@ TigerBeetle.Job = function(request, callback) {
 const create = async function (request, h) {
   LEV(`prepare payload: ${JSON.stringify(request.payload)}`)
   TigerBeetle.create(request, function() {})
-  return h.response().code(202)
+  return h.response().code(203) // Test whether code changes here are effective through to our test harness
   // await TransferService.prepare(request.headers, request.dataUri, request.payload, span)
   // const fspiopError = ErrorHandler.Factory.reformatFSPIOPError(err)
   // Logger.error(fspiopError)
